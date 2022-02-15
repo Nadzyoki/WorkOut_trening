66a6465b8ca13b066a78e67775c63f85c3af4d19
Nadzyoki <atulupev1213@gmail.com>
2022-02-16 00:36:32 +0300

    Start

---
+++ main.py
@@ -0,0 +1,6 @@
+import smtplib
+from email import encoders
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from bs4 import BeautifulSoup as bs
