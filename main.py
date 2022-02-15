78a3898010b391521670914581f3594cb17d024a
Nadzyoki <atulupev1213@gmail.com>
2022-02-16 01:03:18 +0300

    Lets go

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from bs4 import BeautifulSoup as bs
+
+import tkinter as tk
+
+window = tk.Tk()
+greeting = tk.Label(text="Привет, тут будет прога для контроля за отжиманиями с уведомлениями для одобрения твоей работы")
+greeting.pack()
+window.mainloop()
